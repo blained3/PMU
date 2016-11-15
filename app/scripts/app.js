a6bc65f50c396c9dc393aaed1baf5e2ee93559bd
Blaine Backman <backmanb1@gmail.com>
2016-11-15 13:55:12 -0700

    Initial Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name testApp
+ * @description
+ * # testApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('testApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/info', {
+        templateUrl: 'views/info.html',
+        controller: 'InfoCtrl',
+        controllerAs: 'info'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/info.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testApp.controller:InfoCtrl
+ * @description
+ * # InfoCtrl
+ * Controller of the testApp
+ */
+angular.module('testApp')
+  .controller('InfoCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
