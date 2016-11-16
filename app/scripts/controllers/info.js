2c582364507251fb29c25adcef0923b86bb0712c
Aaron Monson <aaron@lmonson.com>
2016-11-16 08:46:26 -0700

    basic info page display

--- app/scripts/controllers/info.js
+++ app/scripts/controllers/info.js
@@ -8,10 +8,12 @@
  * Controller of the testApp
  */
 angular.module('testApp')
-  .controller('InfoCtrl', function () {
+  .controller('InfoCtrl', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
   });
