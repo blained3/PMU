855cac44969e529b79af0e4a4355d9c51a317e79
Blaine Backman <backmanb1@gmail.com>
2016-11-17 09:50:38 -0700

    Added markers

--- app/scripts/controllers/info.js
+++ app/scripts/controllers/info.js
@@ -13,10 +13,44 @@ angular.module('testApp')
 
 		var mapOptions = {
 			center: latLng,
-			zoom: 11,
+			zoom: 13,
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
 
 		this.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    // Place a draggable marker on the map
+    this.markerStart = new google.maps.Marker({
+        position: latLng,
+        map: this.map,
+        draggable:true,
+        icon: `http://maps.google.com/mapfiles/ms/icons/green-dot.png`
+    });
+
+    this.markerEnd = new google.maps.Marker({
+        position: new google.maps.LatLng(40.24, -111.64),
+        map: this.map,
+        draggable:true,
+        icon: `http://maps.google.com/mapfiles/ms/icons/red-dot.png`
+    });
+
+    var infowindow = new google.maps.InfoWindow({
+      content: 'Start!'
+    });
+
+    var infowindow2 = new google.maps.InfoWindow({
+      content: 'End!'
+    });
+
+    infowindow.open(this.map, this.markerStart);
+    infowindow2.open(this.map, this.markerEnd);
+
+    google.maps.event.addListener(this.markerStart, "dragend", function(event) { 
+      console.log(event.latLng.lat()); 
+      var lng = event.latLng.lng(); 
+    }); 
+    google.maps.event.addListener(this.markerEnd, "dragend", function(event) { 
+      console.log(event.latLng.lat()); 
+      var lng = event.latLng.lng(); 
+    }); 
 
   });
