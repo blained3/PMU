9908269f9ab2ce10f3dca14445a6427c5e345ec2
Aaron Monson <aaron@lmonson.com>
2016-11-20 11:21:15 -0700

    Profile page now displays and navigates to upcoming rides

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -8,11 +8,44 @@
  * Controller of the testApp
  */
 angular.module('testApp')
-    .controller('ProfileCtrl', function ($scope, LoginService, $location) {
+    .controller('ProfileCtrl', function ($scope, LoginService, $location, $firebaseArray, IDService) {
         if(!LoginService.isLoggedIn()){
             $location.path('/login');
         }
-        $scope.goToStatusPage = function(){
+        $scope.goToStatusPage = function(ride){
+            IDService.setID(ride.$id);
         	$location.path("/status");
+        };
+
+        var rootRef = firebase.database().ref();
+        var userRef = rootRef.child('testUser');
+        //Eventually, we will want to use whichever activue user it is
+        
+        var loaded = false;
+        $scope.ridesLoaded = function() {
+            return loaded;
+        };
+
+        // download the data into a local object
+        $scope.rides = $firebaseArray(userRef);
+        $scope.rides.$loaded(function(data) {
+            loaded = true;
+        });
+
+        $scope.futureRides = function() {
+            var upcoming = [];
+
+            if (loaded) {
+                var now = Date.now();
+                for (var i = 0; i < $scope.rides.length; i++) {
+                    var ride = $scope.rides[i];
+                    var time = Date.parse(ride.time);
+                    if (time > now) {
+                        upcoming.push(ride);
+                    }
+                }
+            }
+
+            return upcoming;
         };
     });
